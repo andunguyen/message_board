b9c7f097c05c5145d56645c97eeeba8024b09a29
andunguyen <andunguyen.ucsd@gmail.com>
2022-03-05 08:29:25 -0800

    Add a test to account; Reorganize test files; Add background for account pages

--- accounts/tests/tests.py
+++ accounts/tests/tests.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
 from django.urls import reverse, resolve
 from django.test import TestCase
-from .views import signup
 
-from .forms import SignUpForm
+from ..views import signup
+from ..forms import SignUpForm
 
 
 class SignUpTests(TestCase):
@@ -26,6 +26,16 @@ class SignUpTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get('form')
         self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 
 class SuccessfulSignUpTests(TestCase):
@@ -77,3 +87,11 @@ class InvalidSignUpTests(TestCase):
 
     def test_dont_create_user(self):
         self.assertFalse(User.objects.exists())
+
+        
+class SignUpFormTest(TestCase):
+    def test_form_has_fields(self):
+        form = SignUpForm()
+        expected = ['username', 'email', 'password1', 'password2',]
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
