887fc4e7ac301112c56c401630850aafb312c433
andunguyen <andunguyen.ucsd@gmail.com>
2022-03-06 08:33:40 -0800

    Finish up login required new topic view and add a test

--- boards/views.py
+++ boards/views.py
@@ -20,25 +20,19 @@ def board_topics(request, pk):
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
 
-    # TODO: get the currently
-    user = User.objects.first()
-
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
-
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
-
             Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
-            return redirect('board_topics', pk=board.pk)
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
         form = NewTopicForm()
-
     return render(request, 'new_topic.html', {'board': board, 'form': form})
